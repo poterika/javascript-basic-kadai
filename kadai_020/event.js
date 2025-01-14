81217f91a6db62cd58f352d5e3bc00262c139bac
poterika <tennissssr@gmail.com>
2025-01-14 23:00:13 +0900

    kadai_020を追加

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const Btn = document.getElementById('btn');
+// textというidを持つHTML要素を取得し、定数に代入する
+const Text = document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+Btn.addEventListener('click', ()=> {
+ // textを書き換える処理を行う
+  text.textContent='ボタンをクリックしました'
+});
